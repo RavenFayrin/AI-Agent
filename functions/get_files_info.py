efe489077231d6624b8cf3631d4a7bb64e180e70
RavenFayrin <raven.fayrin@gmail.com>
2025-11-10 18:01:48 -0500

    Half working get_files_info

---
+++ functions/get_files_info.py
@@ -0,0 +1,19 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+    if not full_path.startswith(working_directory):
+        raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    if not os.path.isdir(full_path):
+        raise Exception(f'Error: "{directory}" is not a directory')
+    
+    try:
+        contents = os.listdir(full_path)
+        dir_contents = ""
+        for content in contents:
+            file_size = os.path.getsize(os.path.join(working_directory, content))
+            is_dir = os.path.isdir(os.path.join(working_directory, content))
+            dir_contents += "".join(f"- {content}: file_size={file_size}, is_dir={is_dir}\n")
+        return dir_contents.strip()
+    except Exception as e:
+        return f"Error: {e}"
---
+++ tests.py
@@ -0,0 +1,13 @@
+from functions.get_files_info import get_files_info
+
+print("Result for current directory:")
+print(f"{get_files_info("calculator", ".")}\n")
+
+print("Result for 'pkg' directory:")
+print(f"{get_files_info("calculator", "pkg")}\n")
+
+print("Result for '/bin' directory:")
+print(f"{get_files_info("calculator", "/bin")}\n")
+
+print("Result for '../' directory:")
+print(f"{get_files_info("calculator", "../")}\n")
