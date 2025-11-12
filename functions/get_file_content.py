4f042d4d73ab5b08d0dfcb82d7569f69be37032e
RavenFayrin <raven.fayrin@gmail.com>
2025-11-12 15:44:04 -0500

    More Progress on get file content.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,6 @@
 import os
+from functions.config import MAX_CHARS
+
 
 
 def get_file_content(working_directory, file_path):
@@ -9,4 +11,7 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(target_dir):
         return f'Error: "{file_path}" is not a directory'
     
-    
+    with open(file_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+        if f.read(MAX_CHARS) > 10000:
+            file_content_string.append(f"[...File {file_path} truncated at 10000 characters]")
