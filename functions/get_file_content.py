271f611a55df6b41a59ae6d85388ab0b728a30a5
RavenFayrin <raven.fayrin@gmail.com>
2025-11-17 17:20:41 -0500

    copied to match lesson.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,22 +1,21 @@
 import os
-from functions.config import MAX_CHARS
-
+from config import MAX_CHARS
 
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    target_dir = os.path.abspath(os.path.join(working_directory, file_path))
-    if not target_dir.startswith(abs_working_dir):
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(target_dir):
-        return f'Error: "{file_path}" is not a directory'
-    
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
-        with open(target_dir, "r") as f:
-            file_content_string = f.read(MAX_CHARS+1)
-            if len(file_content_string) > MAX_CHARS:
-                truncated_file = file_content_string[:MAX_CHARS] + f"[...File {file_path} truncated at 10000 characters]"
-                return truncated_file
-            return file_content_string
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
     except Exception as e:
-        return f"Error: {e}"
+        return f'Error reading file "{file_path}": {e}'
--- tests.py
+++ tests.py
@@ -2,7 +2,19 @@ from functions.get_file_content import get_file_content
 
 
 def test():
-    result = get_file_content("calculator", "lorem.txt")
+    result = get_file_content("calculator", "main.py")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
     print(result)
     print("")
 
