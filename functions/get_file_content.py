e0731debe9a97385e60bb621ff243f4b75bf3e03
RavenFayrin <raven.fayrin@gmail.com>
2025-12-29 16:54:03 -0500

    Boots said get file content  schema was good. Removed "#" from main.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -23,14 +23,15 @@ def get_file_content(working_directory, file_path):
         return f'Error reading file "{file_path}": {e}'
     
 schema_get_file_content = types.FunctionDeclaration(
-    name="get_files_content",
-    description="Read files in the specified directory, constrained to the working directory.",
+    name="get_file_content",
+    description=f"Reads the content of the requested file up to a {MAX_CHARS} characters limit, file must be within the working directory.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
+        required=["file_path"],
         properties={
-            "directory": types.Schema(
+            "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The directory to read files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                description="The requested file path to the file whose contents are to be returned, relative to the working directory.",
             ),
         },
     ),
