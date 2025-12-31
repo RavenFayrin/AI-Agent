40cf414b7fe1ed989baa9ce1f86547cd5ee2a0ae
RavenFayrin <raven.fayrin@gmail.com>
2025-12-30 19:01:41 -0500

    Made files match lessons

--- functions/write_file_content.py
+++ functions/write_file_content.py
@@ -4,18 +4,14 @@ from google.genai import types
 
 
 def write_file(working_directory, file_path, content):
-    abs_working_dir = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
-    if not abs_file_path.startswith(abs_working_dir):
-        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-    if not os.path.exists(abs_file_path):
-        try:
-            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
-        except Exception as e:
-            return f"Error: creating directory: {e}"
-    if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
-        return f'Error: "{file_path}" is a directory, not a file'
     try:
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(abs_file_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
         with open(abs_file_path, "w") as f:
             f.write(content)
         return (
@@ -23,22 +19,23 @@ def write_file(working_directory, file_path, content):
         )
     except Exception as e:
         return f"Error: writing to file: {e}"
-    
+
+
 schema_write_file = types.FunctionDeclaration(
     name="write_file",
-    description="Write or overwrite a file within the working directory.",
+    description="Writes text content to a specified file within the working directory (overwriting if the file exists)",
     parameters=types.Schema(
         type=types.Type.OBJECT,
-        required=["file_path", "content"],
         properties={
             "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The file location in which the content should be written, relative to the working directory."
+                description="Path to the file to write, relative to the working directory",
             ),
             "content": types.Schema(
                 type=types.Type.STRING,
-                description="The string that should be written into the file."
+                description="Text content to write to the file",
             ),
         },
+        required=["file_path", "content"],
     ),
 )
